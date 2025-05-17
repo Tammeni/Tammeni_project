b0bc4e7b397e3e930414934d7caa8cf8ca6b9ee2
Tammeni <tammeni25@gmail.com>
2025-05-17 20:22:25 +0300

    Update tammini_project.py

--- tammini_project.py
+++ tammini_project.py
@@ -36,12 +36,13 @@ def clean_text(text):
     cleaned = re.sub(r"[\'\"\n\d,;.،؛.؟]", ' ', text)
     cleaned = re.sub(r"\s{2,}", ' ', cleaned)
     emoji_pattern = re.compile("["
-    u"\U0001F600-\U0001F64F"  # emoticons
-    u"\U0001F300-\U0001F5FF"  # symbols
-    u"\U0001F680-\U0001F6FF"  # transport
-    u"\U0001F1E0-\U0001F1FF"  # flags
-    u"\U00002702-\U000027B0"
-    u"\U000024C2-\U0001F251"]+", flags=re.UNICODE)
+u"\U0001F600-\U0001F64F"  # emoticons
+u"\U0001F300-\U0001F5FF"  # symbols
+u"\U0001F680-\U0001F6FF"  # transport
+u"\U0001F1E0-\U0001F1FF"  # flags
+u"\U00002702-\U000027B0"
+u"\U000024C2-\U0001F251"]+", flags=re.UNICODE)
+ 
     cleaned = emoji_pattern.sub(r'', cleaned)
     cleaned = re.sub(r'[\u064B-\u0652]', '', cleaned)
     cleaned = re.sub(r'[إأآا]', 'ا', cleaned)
