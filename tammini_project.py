351a80b301d5262da19b1e9c31d45fdf159927ff
Tammeni <tammeni25@gmail.com>
2025-05-20 00:08:48 +0300

    Update tammini_project.py

--- tammini_project.py
+++ tammini_project.py
@@ -32,7 +32,7 @@ svm_anx = joblib.load("SVM_ANXIETY_FINAL.pkl")
 from sentence_transformers import SentenceTransformer
 import os
 
-model_path = os.path.join(os.getcwd(), 'sbert_model1')
+model_path = os.path.join(os.getcwd(), 'sbert_model4')
 
 # Force CPU loading — avoids .to(device) crash
 Sbert = SentenceTransformer(model_path)
